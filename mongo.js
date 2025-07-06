992e573c905941f8842f6db939033a9b31a80375
Felisuco092 <felisuco146@gmail.com>
2025-07-06 14:22:08 +0200

    Exercise 3.12 mongoDB

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give at leat password')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://felisuco146:${password}@cluster0.7p1ce53.mongodb.net/phoneBookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name : String,
+    number : String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length == 3) {
+    Person.find({}).then(result => {
+        console.log("phoneBook:")
+        result.forEach(person => console.log(`${person.name} ${person.number}`))
+        mongoose.connection.close()
+    })
+} else {
+    const person = new Person({
+        name : process.argv[3],
+        number : process.argv[4],
+    })
+
+    person.save().then(result => {
+        console.log(`added ${process.argv[3]} ${process.argv[4]}`)
+        mongoose.connection.close()
+    })
+}
