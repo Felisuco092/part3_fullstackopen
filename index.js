ebe20054f4e28c80fde19985d9880e17f135e67e
Felisuco092 <felisuco146@gmail.com>
2025-07-07 16:19:14 +0200

    Exercise 3.19 change to backend and frontend in dist

--- index.js
+++ index.js
@@ -101,8 +101,10 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
+    return response.status(400).json({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   return response.status(404).end()
 }
